4f4183de7c08d9347b519f323fa69c5fd9272f47
athirson <athirson@athirson-dev>
2024-03-17 21:01:55 -0300

    improved form and data refreshing and error handling

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/log"
 	"html/template"
 	"io"
 	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 type Templates struct {
@@ -40,6 +42,15 @@ type Data struct {
 	Contacts Contacts
 }
 
+func (d *Data) HasEmail(email string) bool {
+	for _, c := range d.Contacts {
+		if c.Email == email {
+			return true
+		}
+	}
+	return false
+}
+
 func NewData() Data {
 	return Data{
 		Contacts: []Contact{
@@ -49,25 +60,62 @@ func NewData() Data {
 	}
 }
 
+type FormData struct {
+	Values map[string]string
+	Errors map[string]string
+}
+
+func NewFormData() FormData {
+	return FormData{
+		Values: make(map[string]string),
+		Errors: make(map[string]string),
+	}
+}
+
+type Page struct {
+	Data Data
+	Form FormData
+}
+
+func NewPage() Page {
+	return Page{
+		Data: NewData(),
+		Form: NewFormData(),
+	}
+}
+
 func main() {
 	e := echo.New()
 	e.Renderer = NewTemplates()
 	e.Use(middleware.Logger())
-
-	data := NewData()
+	page := NewPage()
 
 	e.GET("/", func(c echo.Context) error {
-		return c.Render(http.StatusOK, "index", data)
+		return c.Render(http.StatusOK, "index", page)
 	})
 
 	e.POST("/contacts", func(c echo.Context) error {
 		name := c.FormValue("name")
 		email := c.FormValue("email")
 
-		newContact := NewContact(name, email)
-		data.Contacts = append(data.Contacts, newContact)
+		if page.Data.HasEmail(email) {
+			formData := NewFormData()
+			formData.Values["name"] = name
+			formData.Values["email"] = email
+			formData.Errors["email"] = "Email already exists"
+			page.Form = formData
 
-		return c.Render(http.StatusOK, "display", data)
+			return c.Render(http.StatusUnprocessableEntity, "form", page.Form)
+		}
+
+		newContact := NewContact(name, email)
+		page.Data.Contacts = append(page.Data.Contacts, newContact)
+
+		err := c.Render(http.StatusOK, "form", NewFormData())
+		if err != nil {
+			log.Error(err)
+		}
+		return c.Render(http.StatusOK, "oob-contact", newContact)
 	})
 
 	e.Logger.Fatal(e.Start(":7777"))
